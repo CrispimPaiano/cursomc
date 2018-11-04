7711b1e4aff6f0579ca5fd26a934e90cbd6a0d9c
CrispimPaiano <crispimpds5@gmail.com>
2018-11-04 20:12:15 -0200

    Produto e associacao muitos para muitos

---
+++ src/main/java/com/crispim/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.crispim.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.crispim.cursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer>{	
+	
+}
